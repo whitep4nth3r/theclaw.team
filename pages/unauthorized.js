f3f0faafbb2433dba90d3784897d98fe5f06e052
Salma Alam-Naylor <whitep4nth3r@gmail.com>
2021-09-30 14:24:33 +0100

    :sparkles: Added styles and other things

--- pages/unauthorized.js
+++ pages/unauthorized.js
@@ -1,13 +1,17 @@
 import { NextSeo } from "next-seo";
 import Layout from "@components/Layout";
 import PageTitle from "@components/PageTitle";
+import typography from "@styles/Typography.module.css";
 
 export default function Home() {
   return (
     <>
       <NextSeo title="Unauthorized" description="Unauthorized" />
       <Layout>
-        <PageTitle title="Unauthorized" />
+        <PageTitle title="403: Access denied" />
+        <p className={typography.typography__body}>
+          Sorry about that! Log in via Twitch is only available to The Claw Stream Team members.
+        </p>
       </Layout>
     </>
   );
