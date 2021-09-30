59e2279dcfdb885d790ae8fce7c104051c8cd3bb
Salma Alam-Naylor <whitep4nth3r@gmail.com>
2021-09-30 15:05:14 +0100

    :bug: Fix sitemap and robots.txt bugs

--- next-sitemap.js
+++ next-sitemap.js
@@ -1,19 +1,14 @@
 module.exports = {
   siteUrl: "https://theclaw.team",
   generateRobotsTxt: true, // (optional)
-  robotsTxtOptions: {
-    policies: [
-      {
-        userAgent: "*",
-        disallow: "/api",
-        disallow: "/404",
-        disallow: "/backstage",
-        disallow: "/unauthorized",
-      },
-    ],
-  },
   exclude: ["/api/*", "404", "/server-sitemap.xml", "/unauthorized", "/backstage"],
   robotsTxtOptions: {
     additionalSitemaps: ["https://theclaw.team/server-sitemap.xml"],
+    policies: [
+      {
+        userAgent: "*",
+        disallow: ["/api", , "/404", "/backstage", "/unauthorized"],
+      },
+    ],
   },
 };
