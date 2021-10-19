70a8dd4131f2dcb1dc4d9876c1ffe665de4eb739
Matt Deloughry <matt@deloughry.co.uk>
2021-10-19 15:08:28 +0100

    :sparkles: Add vacation check and general refactor

--- pages/backstage/overlays/whos-next.js
+++ pages/backstage/overlays/whos-next.js
@@ -3,13 +3,13 @@ import styles from "@styles/Overlays.module.css";
 import { motion } from "framer-motion";
 import TeamData from "@utils/TeamData";
 import StreamerProfileImage from "@components/StreamerProfileImage";
-import {getTimeRemaining} from "@utils/Date";
+import { getTimeRemaining } from "@utils/Date";
 import Countdown from "react-countdown";
 
-export default function OverlayWhosNext({nextUp, avatar, isPartner}) {
+export default function OverlayWhosNext({ nextUp, avatar, isPartner }) {
   const [session] = useSession();
 
-  const {broadcast_name, start_time, title} = nextUp
+  const { broadcast_name, start_time, title } = nextUp;
 
   return (
     <div className={styles.overlay__container}>
@@ -37,42 +37,19 @@ export default function OverlayWhosNext({nextUp, avatar, isPartner}) {
               ease: "easeInOut",
               times: [0, 0.5, 1],
               repeat: Infinity,
-            }}
-          >
+            }}>
             <p>Up Next</p>
           </motion.div>
-          <div
-            className={styles.overlay__whosNext__avatar}
-            animate={{
-              y: [0, 4, 0],
-            }}
-            transition={{
-              duration: 2,
-              ease: "easeInOut",
-              times: [0, 0.5, 1],
-              repeat: Infinity,
-            }}>
-              <StreamerProfileImage imageUrl={avatar} isPartner={isPartner} />
+          <div className={styles.overlay__whosNext__avatar}>
+            <StreamerProfileImage imageUrl={avatar} isPartner={isPartner} />
           </div>
-          <div
-            className={styles.overlay__whosNext__details}
-            animate={{
-              y: ["-200%", "0%", "0%", "-200%"],
-            }}
-            transition={{
-              duration: 10,
-              ease: "easeIn",
-              times: [0, 0.05, 0.95, 1],
-              repeat: Infinity,
-            }}
-            >
-
+          <div className={styles.overlay__whosNext__details}>
             <p className={styles.overlay__whosNext__details__streamer}>
               {broadcast_name} in&nbsp;
               <Countdown date={start_time} />
             </p>
             <p className={styles.overlay__whosNext__details__title}>{title}</p>
-            </div>
+          </div>
         </motion.div>
       </div>
     </div>
@@ -95,17 +72,19 @@ export async function getServerSideProps(context) {
 
   // Check through the next up data and exlude any where the start_time passes
   // Assign to the next up
-   nextUpArr = nextUpArr.filter(streamer => new Date(streamer.start_time) >= new Date());
+  nextUpArr = nextUpArr.filter(
+    (streamer) => new Date(streamer.start_time) >= new Date() && !streamer.vacation,
+  );
 
   const nextUp = nextUpArr[0] ?? null;
 
-  const streamer = await TeamData.getStreamer(nextUp.broadcast_name.toLowerCase())
+  const streamer = await TeamData.getStreamer(nextUp.broadcast_name.toLowerCase());
 
   return {
     props: {
       nextUp,
       avatar: streamer.profile_image_url,
-      isPartner: streamer.broadcaster_type && streamer.broadcaster_type == "partner",
+      isPartner: streamer.broadcaster_type && streamer.broadcaster_type === "partner",
       session: await getSession(context),
     },
   };
