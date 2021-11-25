7488e97ccad996e46a623b48014a652e8dc5f99d
Salma Alam-Naylor <whitep4nth3r@gmail.com>
2021-11-25 15:36:08 +0000

    :sparkles: Add banner to header and move log in to bottom

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -1,6 +1,12 @@
 import Styles from "@styles/Footer.module.css";
+import LoggedInUser from "@components/LoggedInUser";
 
 export default function Footer() {
   const date = new Date();
-  return <footer className={Styles.footer}>&copy; The Claw {date.getFullYear()}</footer>;
+  return (
+    <footer className={Styles.footer}>
+      <LoggedInUser />
+      <p className={Styles.footer__copyright}>&copy; The Claw {date.getFullYear()}</p>
+    </footer>
+  );
 }
