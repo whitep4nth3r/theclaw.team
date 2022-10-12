847066091884413dc4bde8950708756548ab82bc
@whitep4nth3r <52798353+whitep4nth3r@users.noreply.github.com>
2022-10-12 12:15:05 +0100

    Update raidathon overlay

--- pages/backstage/overlays/raidathon.js
+++ pages/backstage/overlays/raidathon.js
@@ -1,67 +1,17 @@
-import { useEffect, useState } from "react";
-import { useSession, getSession, signIn } from "next-auth/react";
+import { getSession } from "next-auth/react";
 import OverlayStyles from "@styles/Overlays.module.css";
 import styles from "@styles/Raidathon.module.css";
-import classNames from "classnames/bind";
 import Moth from "@components/Svg/Moth";
-// import FlowerRed from "@components/Svg/FlowerRed";
-// import FlowerBlue from "@components/Svg/FlowerBlue";
-// import { motion } from "framer-motion";
-
-let cx = classNames.bind(styles);
 
 export default function Raidathon() {
-  const { data: session } = useSession();
-  const [isAnimating, setIsAnimating] = useState(true);
-
-  useEffect(() => {
-    setInterval(() => {
-      const newValue = !isAnimating;
-      setIsAnimating(newValue);
-    }, 60000);
-
-    return () => {};
-  }, [isAnimating]);
-
   return (
     <div className={OverlayStyles.overlay__container}>
-      <span className={styles.joinDiscord__checkOut}>
-        Check out
-        <span role="img" aria-label="hand pointing down">
-          👇
-        </span>
-      </span>
-      {/* <span className={styles.flowerBlue}>
-        <FlowerBlue />
-      </span>
-      <span className={styles.flowerRed}>
-        <FlowerRed />
-      </span> */}
-
       <div className={styles.moth}>
         <Moth />
       </div>
 
-      <h1
-        id="text"
-        className={cx({
-          text: true,
-          animate: isAnimating,
-        })}>
-        <span className={styles.blockSeparator}>
-          {"THE CLAW TEAM".split("").map((char, index) => (
-            <span key={`text-${index}`} className={styles.letter} style={{ "--index": index }}>
-              {char}
-            </span>
-          ))}
-        </span>
-        <span className={styles.blockSeparator}>
-          {"RAIDATHON".split("").map((char, index) => (
-            <span key={`text2-${index}`} className={styles.letter} style={{ "--index": index + 2 }}>
-              {char}
-            </span>
-          ))}
-        </span>
+      <h1 id="text" className={styles.text}>
+        RAIDATHON
       </h1>
     </div>
   );
