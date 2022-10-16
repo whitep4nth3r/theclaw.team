7104dffbdb9900d5489e82287d10a0090644c529
@whitep4nth3r <52798353+whitep4nth3r@users.noreply.github.com>
2022-10-16 14:31:50 +0100

    Add countdown to radiathon overlay

--- pages/backstage/overlays/raidathon.js
+++ pages/backstage/overlays/raidathon.js
@@ -1,9 +1,20 @@
+import { useState, useEffect } from "react";
 import { getSession } from "next-auth/react";
 import OverlayStyles from "@styles/Overlays.module.css";
 import styles from "@styles/Raidathon.module.css";
 import Moth from "@components/Svg/Moth";
+import Countdown from "react-countdown";
 
 export default function Raidathon() {
+  const raidathonEnd = "2022-10-21T23:00:00Z";
+  const date = new Date(raidathonEnd);
+  const [showCountdown, setShowCountdown] = useState(false);
+
+  useEffect(() => {
+    setShowCountdown(true);
+    return () => {};
+  }, [setShowCountdown]);
+
   return (
     <div className={OverlayStyles.overlay__container}>
       <div className={styles.moth}>
@@ -11,7 +22,8 @@ export default function Raidathon() {
       </div>
 
       <h1 id="text" className={styles.text}>
-        RAIDATHON
+        RAIDATHON <br />
+        {showCountdown && <Countdown date={date} />}
       </h1>
     </div>
   );
